10a9f1182e8c918d8383601fdbba6d2c80e12db3
Prajit <praj94@hotmail.com>
2018-10-11 12:10:01 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,115 @@
+const SlackBot = require('slackbots');
+const axios = require('axios');	
+
+const bot = new SlackBot({
+	token:'xoxb-453845983106-454905269719-UQgAH8YVVysdaPw156LCCumB',
+	name: 'jokebot'
+});
+
+// Start Handler 
+bot.on('start',() =>{
+	const params = {
+		icon_emoji: ':smiley:'
+
+	}
+
+	bot.postMessageToChannel('general','Get Ready To Laugh with @Jokebot',params);
+
+});
+
+//Error Handler
+bot.on('error',(err) => console.log(err));
+
+// Message Handler 
+bot.on('message',(data) => {
+	if(data.type !== 'message') {
+		return;
+	}
+
+
+	handleMessage(data.text);
+
+});
+
+// Respond to Data
+function handleMessage(message) {
+	if(message.includes(' chucknorris')) {
+		chuckJoke();
+
+	} else if(message.includes(' yomama')){
+		yoMamaJoke();
+	} else if (message.includes('random')){
+		randomJoke();
+	} else if (message.includes('help')){
+		runHelp();
+	}
+
+
+}
+
+//Tell a Chuck Norris Joke
+function chuckJoke() {
+	axios.get('http://api.icndb.com/jokes/random')
+	.then(res => {
+		const joke = res.data.value.joke;
+
+		const params = {
+		icon_emoji: ':laughing:'
+
+	}; 
+	bot.postMessageToChannel(
+		'general',
+		`Chuck Norris : ${joke}`,
+		params
+
+		);
+
+	})
+}
+
+//Tell a Yo Mama Joke
+function yoMamaJoke() {
+	axios.get('http://api.yomomma.info') 
+	.then(res => {
+		const joke = res.data.joke;
+
+		const params = {
+		icon_emoji: ':laughing:'
+
+	}; 
+	bot.postMessageToChannel(
+		'general',
+		`Yo Mama : ${joke}`,
+		params
+
+		);
+
+	})
+}
+
+// Tell a Random Joke
+function randomJoke() {
+	const rand = Math.floor(Math.random() * 2) + 1;
+	if(rand === 1){
+		chuckJoke();
+
+
+	} else if(rand === 2){
+		yoMamaJoke();
+	}
+}
+
+// SHow Help text
+function runHelp(){
+	const params = {
+		icon_emoji: ':question:'
+
+	}; 
+
+	bot.postMessageToChannel(
+		'general',
+		`Type @jokebot with either 'chucknorris ', 'yomama' or 'random' to get a joke`,
+		params
+
+		); 
+}
